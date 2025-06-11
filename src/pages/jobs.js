0aa8048c71b0b0753bf3fcb88c33b095511e0fd9
Morgan Mason <morganmason0606@gmail.com>
2025-06-11 10:38:47 -0700

    minor change

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -5,11 +5,11 @@ function Job(){
         <>
             <h1>Job Experience</h1>
             <div class="card_list">
-                <div className="display_card">
+                {/* <div className="display_card">
                     <h2>AI Research and Development – Credit Risk Intern</h2>
                     <h3>US Bank, Summer 2024</h3>
                     <p>Conducted comprehensive research on AI use cases within the banking sector. Developed <a href="https://github.com/morganmason0606/LLM_Agents">an advanced chatbot</a> utilizing credit-risk model documentation to streamline the process for senior management in addressing onsite regulatory examination inquiries regarding model outputs. <a href="morganmason0606.github.io/excel_joiner/">Automated the collection and reporting</a> of profitability and asset quality ratios across bank competitors, reducing lead times for senior management to benchmark performance against peer banks. </p>
-                </div>
+                </div> */}
                 <div class="display_card">
                     <h2>Tech Support</h2>
                     <h3>Acalanes Union High School District, Summer 2021</h3>
